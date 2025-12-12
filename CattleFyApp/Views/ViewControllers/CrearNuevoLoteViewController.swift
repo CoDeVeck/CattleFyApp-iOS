3837dca1315541929d702bb1c1f44e2ebe29179c
Andy Valdivia Centeno <andy.v.centeno@gmail.com>
2025-12-12 13:36:52 -0800

    feat: agregar vistas iniciales para creación de lotes, registro masivo y funcionalidades de alimentación y sanitario(avance parcial)

---
+++ CattleFyApp/Views/ViewControllers/CrearNuevoLoteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CrearNuevoLoteViewController.swift
+//  
+//
+//  Created by Andy Yahir Valdivia Centeno on 11/12/25.
+//
+
+import UIKit
+
+class CrearNuevoLoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ CattleFyApp/Views/ViewControllers/HistorialAlimentacionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HistorialAlimentacionViewController.swift
+//
+//
+//  Created by Andy Yahir Valdivia Centeno on 11/12/25.
+//
+
+import UIKit
+
+class HistorialAlimentacionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
