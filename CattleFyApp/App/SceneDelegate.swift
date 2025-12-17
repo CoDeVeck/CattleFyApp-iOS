52d33915c7454687b85a00c1ef24f38594f996d9
victor <victornarazas2005@gmail.com>
2025-12-17 11:07:16 -0500

    Arreglando vistas

--- CattleFyApp/App/SceneDelegate.swift
+++ CattleFyApp/App/SceneDelegate.swift
@@ -12,7 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-<<<<<<< HEAD
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
@@ -32,15 +31,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 
-=======
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-
->>>>>>> c57ccc11a6c3df26f8186cdb314b7c120edd3a90
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -74,4 +64,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
--- CattleFyApp/Views/ViewControllers/VentaComercialViewController.swift
+++ CattleFyApp/Views/ViewControllers/VentaComercialViewController.swift
@@ -1,16 +1,7 @@
-//
-//  VentaComercialViewController.swift
-//  CattleFyApp
-//
-//  Created by Victor  on 10/12/25.
-//
-
 import UIKit
 
 class VentaComercialViewController: UIViewController {
 
-<<<<<<< HEAD
-    
     @IBOutlet weak var containerView: UIView!
     
   
@@ -65,23 +56,3 @@ class VentaComercialViewController: UIViewController {
 
 }
 
-=======
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
->>>>>>> c57ccc11a6c3df26f8186cdb314b7c120edd3a90
