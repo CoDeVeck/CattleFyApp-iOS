3616c70aca40d6dddd6f245f44af1f7d79463f05
Rebeca Yllanes <rebeca.yllanes.ch@gmail.com>
2025-12-18 22:41:01 -0500

    Se mejoraron las vistas y sus constraints
    diosmio salvame

--- CattleFyApp/Views/ViewControllers/LoginViewController.swift
+++ CattleFyApp/Views/ViewControllers/LoginViewController.swift
@@ -58,8 +58,8 @@ class LoginViewController: UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         
         guard let inicioVC = storyboard.instantiateViewController(
-            withIdentifier: "PerfilUsuarioViewController"
-        ) as? CrearNuevoLoteViewController else {
+            withIdentifier: "OnboardingPaso1ViewController"
+        ) as? OnboardingPaso1ViewController else {
             print("Error: No se pudo castear InicioViewController")
             mostrarAlerta(mensaje: "Error al cargar la pantalla principal")
             return
